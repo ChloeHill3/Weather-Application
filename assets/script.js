ad7d3643d55dd045dc7794ba295e72f04ece7786
Chloe Hill <chloehill333@gmail.com>
2023-03-24 23:37:55 +0000

    js not functioning - bug needs fixing

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,67 @@
 let today = moment().format("DD-MM-YYYY");
-var cityName = document.querySelector("#search-input").value;
-const myApiKey = "c9e8960cfd8a13fba033eae63434578c";
-let cities = [];
+const myApiKey = '1ffa6d2a9e2efff932f3fc6da58adb78';
+const weatherWrapper = document.querySelector("#today");
+const forecastWrapper = document.querySelector("#forecast");
+const forecastContainer = document.querySelector("#forecast-list");
+const searchHistory = document.querySelector("#search-history");
+
+function fetchWeather(city, myApiKey) {
+
+  $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myApiKey}`)
+   .then((list) => {
+    var lon = list.coord.lon;
+    var lat = list.coord.lat;
+
+    
+    displayWeather(list);
+   }
+  );
+}
+
+function displayWeather(currentData) {
+  console.log(`
+   City: ${currentData.name}
+   Temp: ${Math.round(currentData.main.temp)} Cº
+   Wind: ${currentData.wind.speed} M/S
+   Humidity: ${currentData.main.humidity}
+   `);
+
+  let city = `${currentData.name}`;
+  let date = moment().format("DD-MM-YYYY");
+  let icon = `${currentData.weather[0].icon}`;
+  let weatherDescription = `${currentData.weather[0].description}`;
+  let temp = `${Math.round(currentData.main.temp)} Cº`;
+  let wind = `${currentData.wind.speed} m/s`;
+  let humidity = `${currentData.main.humidity}%`;
 
 
+  $(`<div id="city-date-icon-wrap">
+           <h2 class="heading radius">${city.name} ${date}</h2>
+           <img src="https://openweathermap.org/img/w/${icon}.png" alt="${weather[0].description}"></img>
+       </div>
+       <p>Temp: ${main.temp}</p>
+       <p>Wind: ${wind.speed}</p>
+       <p>Humidity: ${main.humidity}</p>`).appendTo(weatherWrapper);
 
+  //Show 5 day Forecast
+  $(forecastWrapper).show();
 
+  for (i = 0; i < 5; i++) {
+    date = moment().format("DD-MM-YYYY");
+    temp = "";
+    wind = `${3 * i} mph`;
+    humidity = "";
+    $(`<div class="forecast-card radius" id="today">
+               <h3>${date}</h3>
+               <p>${img}</p>
+               <p>${weatherDescription}</p>
+               <p>Temp: ${temp}</p>
+               <p>Wind: ${wind}</p>
+               <p>Humidity: ${humidity}</p>
+           </div>
+       `).appendTo(forecastContainer);
+  }
+}
 
+fetchWeather();
 
-// document.querySelector("#search-button")
-//   .addEventListener("click", function (event, cityName) {
-//     // Preventing the submit button from trying to submit the form
-//     event.preventDefault();
-//     city(cityName);
-//     var lat = data[0].lat;
-//     var lon = data[0].lon;
-//     queryCurrentURL =
-//       "https://api.openweathermap.org/data/2.5/weather?lat=" +
-//       lat +
-//       "&lon=" +
-//       lon +
-//       "&units=metric&appid=c9e8960cfd8a13fba033eae63434578c";
-//     queryForecastURL =
-//       "https://api.openweathermap.org/data/2.5/forecast?lat=" +
-//       lat +
-//       "&lon=" +
-//       lon +
-//       "&units=metric&appid=c9e8960cfd8a13fba033eae63434578c";
-//     queryURL = queryCurrentURL;
-//     return fetch(queryURL);
-
-//     function city(cityName) {
-//       console.log(cityName);
-
-//       queryURL =
-//         "https://api.openweathermap.org/data/2.5/weather?q=" +
-//         cityName +
-//         "&appid=c9e8960cfd8a13fba033eae63434578c";
-
-//       fetch(queryURL)
-//         .then((response) => response.json())
-//         .then((data) => {
-//           let cityNameData = data;
-//           console.log(responseData);
-//           for (let i = 0; i < 20; i++) {
-//             cities[i] = cityNameData[i].city;
-//           }
-//         });
-//     }
-
-//     getLatLong(cityName);
-//   });
